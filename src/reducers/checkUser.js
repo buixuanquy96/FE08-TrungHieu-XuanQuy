a293d9499e4170bce201a1ce495e1dbe149ba2ea
Quy Neo <buixuan.quy96@gmail.com>
2020-01-02 17:03:24 +0700

    first commit

---
+++ src/reducers/checkUser.js
@@ -0,0 +1,45 @@
+
+var initialState = {
+    loggingIn: false
+}
+
+//var initialState = []
+
+const checkUser = (state = initialState, action) => {
+    var {userLogin} = action
+  
+    switch(action.type){
+        case 'CHECK_USER':
+            
+            // console.log("ok")
+            //console.log(state)
+            // state = sessionStorage.setItem('TOKEN', JSON.stringify(state));
+            sessionStorage.setItem('TOKEN', JSON.stringify(userLogin))
+            
+            //return [...state];
+            
+            return {
+                loggingIn: true,
+            };
+        case 'LOGOUT_USER':
+        
+            //console.log("abc")
+            
+            sessionStorage.removeItem('TOKEN', JSON.stringify(userLogin));
+            // //return [...state];
+            
+            return {
+                loggingIn: false,
+            };
+
+        // default: 
+        // return [...state];
+        default:
+            //return [...state];
+            return state
+    }
+}
+
+
+
+export default checkUser; 
